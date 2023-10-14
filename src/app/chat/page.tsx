fe75c7c2ff5bb59d83b6e93e5a4fa751039c2ba1
Patrick Carter <xCarter93@gmail.com>
2023-10-14 11:07:14 -0400

    Included functionality to dynamically fetch user and connect to chats

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,39 +1,44 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import { StreamChat } from "stream-chat";
 import {
   Channel,
   ChannelHeader,
+  ChannelList,
   Chat,
+  LoadingIndicator,
   MessageInput,
   MessageList,
   Thread,
   Window,
 } from "stream-chat-react";
-
-const userId = "user_2WjRNCs0YrqznVl1otGbFtPZmUp";
-
-const chatClient = StreamChat.getInstance(process.env.NEXT_PUBLIC_STREAM_KEY!);
-
-chatClient.connectUser(
-  {
-    id: userId,
-    name: "Patrick Carter",
-  },
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yV2pSTkNzMFlycXpuVmwxb3RHYkZ0UFptVXAifQ.cDfjakC8_30Sdhtwf0vGBKDOHuXAGenA-S243vIB71E"
-);
-
-const channel = chatClient.channel("messaging", "channel_1", {
-  name: "Channel 1",
-  members: [userId],
-});
+import useInitializeChatClient from "./useInitializeChatClient";
 
 export default function ChatPage() {
+  const chatClient = useInitializeChatClient();
+  const { user } = useUser();
+
+  if (!chatClient || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <LoadingIndicator size={40} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Chat client={chatClient}>
-        <Channel channel={channel}>
+        <ChannelList
+          filters={{
+            type: "messaging",
+            members: { $in: [user.id] },
+          }}
+          sort={{ last_message_at: -1 }}
+          options={{ state: true, presence: true, limit: 10 }}
+        />
+        <Channel>
           <Window>
             <ChannelHeader />
             <MessageList />
