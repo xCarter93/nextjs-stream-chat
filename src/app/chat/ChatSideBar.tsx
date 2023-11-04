9800d93845c374584df645079826c3461cbeaa6e
Patrick Carter <xCarter93@gmail.com>
2023-11-04 18:36:47 -0400

    Added additional responsiveness

---
+++ src/app/chat/ChatSideBar.tsx
@@ -0,0 +1,54 @@
+import {
+  ChannelList,
+  ChannelPreviewMessenger,
+  ChannelPreviewUIComponentProps,
+} from "stream-chat-react";
+import MenuBar from "./MenuBar";
+import { UserResource } from "@clerk/types";
+import { useCallback } from "react";
+
+interface ChatSideBarProps {
+  user: UserResource;
+  show: boolean;
+  onClose: () => void;
+}
+
+export default function ChatSideBar({ user, show, onClose }: ChatSideBarProps) {
+  const ChannelPreviewCustom = useCallback(
+    (props: ChannelPreviewUIComponentProps) => (
+      <ChannelPreviewMessenger
+        {...props}
+        onSelect={() => {
+          props.setActiveChannel?.(props.channel, props.watchers);
+          onClose();
+        }}
+      />
+    ),
+    [onClose]
+  );
+  return (
+    <div
+      className={`w-full flex-col md:max-w-[360px] ${show ? "flex" : "hidden"}`}
+    >
+      <MenuBar />
+      <ChannelList
+        filters={{
+          type: "messaging",
+          members: { $in: [user.id] },
+        }}
+        sort={{ last_message_at: -1 }}
+        options={{ state: true, presence: true, limit: 10 }}
+        showChannelSearch
+        additionalChannelSearchProps={{
+          searchForChannels: true,
+          searchQueryParams: {
+            channelFilters: {
+              filters: { members: { $in: [user.id] } },
+            },
+          },
+        }}
+        Preview={ChannelPreviewCustom}
+      />
+    </div>
+  );
+}
